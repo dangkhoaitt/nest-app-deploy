0d7d6fef6ae48b0cf1b8e05fd2d11bff560d1215
KHOA <khoavd@minori.com.vn>
2021-06-04 07:07:00 +0700

    update server

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import * as cors from 'cors';
-import * as Express from 'express';
 import { AppModule } from './app.module';
-
-const server = Express();
-server.use(cors());
-server.get('/', (req, res) => res.send('ok'));
-server.get('/_ah/health', (req, res) => res.send('ok'));
-server.get('/_ah/start', (req, res) => res.send('ok'));
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
